6bbe0725828d20adadca8f860c1898dd88441f91
akihirok2k2 <akihirok2k2@gmail.com>
2018-12-01 20:24:32 +0900

    Fix: ミスった場合の処理を修正

--- bin/speedtest.sh
+++ bin/speedtest.sh
@@ -10,13 +10,11 @@ DATE="$(date "+%Y%m%d-%H%M%S")"
 ##  6766) JAIST(ino-lab) (Nomi, Japan) [279.62 km]
 /usr/local/bin/speedtest --server 6766 --timeout 60 --simple > $TMPFILE
 
-if [ -f "$TMPFILE" ] ;then
-  DOWN_SPEED=$(awk -F: '/Download/{print $2}' $TMPFILE )  
-  UP_SPEED=$(awk -F: '/Upload/{print $2}' $TMPFILE )  
-else 
-  DOWN_SPEED="NG"
-  UP_SPEED="NG"
-fi
+DOWN_SPEED=$(awk -F: '/Download/{print $2}' $TMPFILE )
+UP_SPEED=$(awk -F: '/Upload/{print $2}' $TMPFILE )
+
+[ "$DOWN_SPEED" == "" ] && DOWN_SPEED="NG"
+[ "$UP_SPEED" == "" ] && UP_SPEED="NG"
 
 echo -e "$DATE \t $DOWN_SPEED \t $UP_SPEED " >> $OUTPUT
 
